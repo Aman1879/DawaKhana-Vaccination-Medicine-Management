1e8d35ee773b6ad83f0fe2731629ea6a1e5866ab
AMan <amanara13579@gmail.com>
2026-05-25 20:52:20 +0530

    Update Vite config for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['@react-three/drei'],
-  },
-  server: {
-    port: 5173,
-  },
-});
+  base: '/',
+})
